7bcbff378200c440ab9db379c3ed40fb83e4980e
uclinxiuda <wb-lxd188077@alibaba-inc.com>
2016-05-19 09:34:49 +0800

    Signed-off-by: uclinxiuda <wb-lxd188077@alibaba-inc.com>

--- learnTools/gulpfile.js
+++ learnTools/gulpfile.js
@@ -1,9 +1,51 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
+var plugins = require('gulp-load-plugins')(); //立即执行
+//插件自动加载工具，在需要用到对应插件时就会自动从package.json的依赖中下载
+
+gulp.task('html',function(){
+	gulp.src('html/index.html')
+	.pipe(gulp.dest('test'))
+	.pipe(plugins.livereload());
+})
+
+gulp.task('watch',function(){
+	plugins.livereload.listen();
+	gulp.watch('html/index.html',['html'])
+})
+
 //创建minify压缩任务
-gulp.task('minify', function() {
-	console.log('mytask');
-	gulp.src('js/app.js')//打开当前目录下的相应文件
-		.pipe(uglify())//压缩
-		.pipe(gulp.dest('build'))//写入build目录
+gulp.task('minify', ['minify-js', 'minify-html', 'minify-css'], function() {
+	console.log('All files have already minify!')
+})
+
+gulp.task('minify-js', function() {
+	gulp.src('js/*.js') //打开目录下的相应文件
+		//.pipe(plugins.concat('all.js'))  //合并js文件
+		.pipe(plugins.uglify()) //压缩
+		//.pipe(plugins.rename('app.min.js'))  //多个文件的时候就不要rename了
+		.pipe(gulp.dest('build')) //写入build目录
 });
+
+gulp.task('minify-html', function() {
+	gulp.src('html/*.html')
+		.pipe(plugins.minifyHtml())
+		//.pipe(plu gins.rename('index.min.html'))
+		.pipe(gulp.dest('build'))
+})
+
+gulp.task('minify-css', function() {
+	gulp.src('css/*.css')
+		//.pipe(plugins.concat('all.css'))  //合并css文件
+		.pipe(plugins.minifyCss())
+		//.pipe(plugins.rename('style.min.css'))
+		.pipe(gulp.dest('build'))
+})
+
+//gulp.task('htmljs-uglify',['useref',''])
+
+// gulp.task('useref', function() {
+// 	gulp.src('html/*.html')
+// 		.pipe(plugins.useref())
+// 		.pipe(plugins.rename('html-allJs.js'))
+// 		.pipe(gulp.dest('build'))
+// })
